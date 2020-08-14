5f57be8e1be246d15d28dd7735674131bb95d206
Maureen435 <mchepkemoi435@gmail.com>
2020-08-14 12:23:23 +0300

    modified project urls

--- awards/urls.py
+++ awards/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path,include 
+from django.contrib.auth import views 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('projects.urls')),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings'))
 ]
