07b8a15d84c2f6074eecb7fe662ff99dfd7d9a43
Maureen435 <mchepkemoi435@gmail.com>
2020-08-14 14:26:12 +0300

    modified models

--- projects/models.py
+++ projects/models.py
@@ -12,3 +12,34 @@ class Post(models.Model):
     image = models.ImageField(upload_to='projects/')
     link = models.CharField(max_length=70)
     technologies = models.CharField(max_length=100)
+
+     def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+        
+    @classmethod
+    def search(cls,searchterm):
+        search = Post.objects.filter(Q(title__icontains=searchterm)|Q(desc=searchterm))
+        return search
+
+
+    def get_absolute_url(self): 
+        return reverse('post_detail', args=[str(self.id)])
+
+class Profile(models.Model):
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics/')
+    bio = models.TextField(default="Hello there!")
+    email = models.CharField(blank = True, max_length = 100)
+
+    def __str__(self):
+        return self.image
+
+    def save_profile(self):
+        self.save()
