e40ad534e9961d4be219e62921644a2f8aba4e87
Maureen435 <mchepkemoi435@gmail.com>
2020-08-14 13:57:23 +0300

    modified project views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse_lazy
+from .forms import PostForm,ProfileForm,UserRegisterForm
+from django.views import generic
+from .models import *
+from django.db.models import Avg
+
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'projects/index.html'
