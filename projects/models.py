68824a83f138fdb9d72b2ffbe7fbcc5583be9e9c
Maureen435 <mchepkemoi435@gmail.com>
2020-08-14 14:25:15 +0300

    added classes

--- projects/models.py
+++ projects/models.py
@@ -3,3 +3,12 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.urls import reverse
 from django.db.models import Q
+
+class Post(models.Model):
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    title= models.CharField(max_length=50)
+    desc= models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='projects/')
+    link = models.CharField(max_length=70)
+    technologies = models.CharField(max_length=100)
